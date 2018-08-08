correct gradient

adaptive_softmax.py
@@ -87,9 +87,12 @@ if __name__ == '__main__':
 
     x = tf.to_int32(tf.floor(tf.random_uniform([N, 1], 0, 1) * 0.999 * V))
     y = tf.to_int32(tf.floor(tf.random_uniform([N, 1], 0, 1) * 0.999 * V))
-    weight = tf.Variable(tf.random_normal([V, H], 0, 1))
-    bias = tf.Variable(tf.ones([V]))
+    weight = tf.Variable(tf.random_normal([V, H], 0, 1), name='W')
+    bias = tf.Variable(tf.ones([V]), name='b')
     embed = tf.nn.embedding_lookup(weight, y)
+    test_embed = tf.assign(tf.Variable([]), embed, validate_shape=False)
+    with tf.control_dependencies([test_embed]):
+        embed = tf.identity(embed)
     loss = AdaptiveSoftmaxLoss(H, [0, 4, 8]).apply(
         weight, bias, embed, x, True)
     exp_loss = tf.exp(loss)
@@ -98,6 +101,7 @@ if __name__ == '__main__':
     grads_and_vars = optimizer.compute_gradients(loss)
     train_op = optimizer.apply_gradients(
         grads_and_vars, global_step=global_step)
+    print(grads_and_vars)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for _ in range(E):

model.py
@@ -72,13 +72,15 @@ class LanguageModel():
                                               lambda: self._zero_state,
                                               lambda: self._all_states)
                 else:
-                    next_cell_state = tuple(
-                        LSTMStateTuple(
-                            c=tf.assign(x.c, y.c, validate_shape=False),
-                            h=tf.assign(x.h, y.h, validate_shape=False)
+                    ops = [tf.assign(x.c, y.c, validate_shape=False) for x, y in zip(self._all_states, cell_state)] + [
+                        tf.assign(x.h, y.h, validate_shape=False) for x, y in zip(self._all_states, cell_state)]
+                    with tf.control_dependencies(ops):
+                        next_cell_state = tuple(
+                            LSTMStateTuple(
+                                c=tf.identity(x.c),
+                                h=tf.identity(x.h)
+                            ) for x in cell_state
                         )
-                        for x, y in zip(self._all_states, cell_state)
-                    )
                 elements_finished = (time >= self.seq_lens)
                 finished = tf.reduce_all(elements_finished)
                 next_input = tf.cond(
@@ -97,6 +99,8 @@ class LanguageModel():
 
 if __name__ == '__main__':
     import numpy as np
+    np.random.seed(42)
+    tf.set_random_seed(42)
     V = 50
     model = LanguageModel(
         vocab_size=V,
@@ -108,8 +112,6 @@ if __name__ == '__main__':
         drop_e=0.0
     )
     model.build_model()
-    np.random.seed(42)
-    tf.set_random_seed(42)
     words = np.random.random_integers(low=0, high=V-1, size=(10, 5))
     print(words)
     sess = tf.Session()
@@ -123,7 +125,7 @@ if __name__ == '__main__':
                              model.seq_lens: [10]*5,
                              model.reset_state: i == 0
                          })
-            print('Outputs', o)
+            print('Outputs', j, ':', o)
             n = [n.name for n in tf.get_default_graph().as_graph_def().node]
             print("No.of nodes: ", len(n), "\n")
     print(tf.trainable_variables())

weight_drop_lstm.py
@@ -158,7 +158,6 @@ if __name__ == '__main__':
             h=tf.get_variable(shape=[1, 3], name='state_{}_h'.format(
                 i), trainable=False)) for i in range(len(zero_state))
     )
-    
 
     def loop_fn(time, cell_output, cell_state, loop_state):
         emit_output = cell_output  # == None for time == 0
@@ -167,13 +166,15 @@ if __name__ == '__main__':
                                       lambda: zero_state,
                                       lambda: all_states)
         else:
-            next_cell_state = tuple(
-                LSTMStateTuple(
-                    c=tf.assign(x.c, y.c, validate_shape=False),
-                    h=tf.assign(x.h, y.h, validate_shape=False)
+            ops = [tf.assign(x.c, y.c, validate_shape=False) for x, y in zip(all_states, cell_state)] + [
+                tf.assign(x.h, y.h, validate_shape=False) for x, y in zip(all_states, cell_state)]
+            with tf.control_dependencies(ops):
+                next_cell_state = tuple(
+                    LSTMStateTuple(
+                        c=tf.identity(x.c),
+                        h=tf.identity(x.h)
+                    ) for x in cell_state
                 )
-                for x, y in zip(all_states, cell_state)
-            )
         elements_finished = (time >= seq_len)
         finished = tf.reduce_all(elements_finished)
         next_input = tf.cond(