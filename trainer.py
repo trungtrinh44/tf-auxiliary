acecf47b5184a873e7f021b5f0510eb0027966ef
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2018-08-09 23:06:11 +0700

    update

--- trainer.py
+++ trainer.py
@@ -141,7 +141,7 @@ class Trainer():
             )
             self.train_summaries_writer.add_summary(summaries, step)
             self.logger.info(
-                "Step {0:05d}: len {0:03d},loss {0:.4f}, ppl {0:.4f}, bpc {0:.4f}, time {0:.4f}".format(step, len(next_x), loss, ppl, bpc, time.time()-start_time))
+                "Step {d}: len {d}, loss {0:.4f}, ppl {0:.4f}, bpc {0:.4f}, time {0:.4f}".format(step, len(next_x), loss, ppl, bpc, time.time()-start_time))
             start_time = time.time()
 
             batch += 1
