4973600f52a84c11914ce8176c9532f9a590bc32
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2018-08-09 11:11:59 +0700

    add utils

---
+++ utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def get_batch(source, bptt, i):
+    real_bptt = bptt if np.random.random() < 0.95 else bptt / 2.
+    # Prevent excessively small or negative sequence lengths
+    seq_len = max(5, int(np.random.normal(real_bptt, 5)))
+    seq_len = min(seq_len, len(source) - 1 - i)
+    data = source[i:i+seq_len]
+    target = source[i+1:i+1+seq_len]
+    return data, target
+
+def batchify(source, bsz):
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = len(source) // bsz
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = source[:nbatch * bsz]
+    # Evenly divide the data across the bsz batches.
+    data = np.reshape(data, [bsz, nbatch])
+    return data
