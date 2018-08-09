7ae9b194a5f6967e4010d99a5ed0bface0ffecfb
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2018-08-09 19:30:28 +0700

    fix trainer bug

--- trainer.py
+++ trainer.py
@@ -5,11 +5,10 @@ from model import LanguageModel
 
 
 class Trainer():
-    def __init__(self, model_configs, optimizer, learning_rate, y, alpha, beta, clip_norm, bptt, log_path, train_summary_dir, test_summary_dir, checkpoint_dir, name='LM_Trainer'):
+    def __init__(self, model_configs, optimizer, learning_rate, decay_rate, decay_freq, alpha, beta, clip_norm, bptt, log_path, train_summary_dir, test_summary_dir, checkpoint_dir, name='LM_Trainer'):
         self.model_configs = model_configs
         self.optimizer = optimizer
         self.learning_rate = learning_rate
-        self.y = y
         self.name = name
         self.train_summary_dir = train_summary_dir
         self.test_summary_dir = test_summary_dir
@@ -19,12 +18,17 @@ class Trainer():
         self.bptt = bptt
         self.clip_norm = clip_norm
         self.logger = get_logger(log_path)
+        self.decay_rate = decay_rate
+        self.decay_freq = decay_freq
 
     def build(self):
         self.session = tf.Session()
         self.model_train = LanguageModel(
             **self.model_configs, reuse=False, is_training=True)
+        self.model_train.build_model()
         with tf.variable_scope(self.name):
+            self.y = tf.placeholder(dtype=tf.int32, shape=[
+                                    None, None], name='y')
             self.raw_loss = tf.contrib.seq2seq.sequence_loss(
                 logits=self.model_train.decoder,
                 targets=self.y,
@@ -39,7 +43,7 @@ class Trainer():
                     tf.reduce_sum(tf.square(self.model_train.rnn_outputs)),
                     tf.multiply(
                         tf.reduce_sum(self.model_train.expand_seq_masks),
-                        self.model_train.rnn_outputs.shape[-1]
+                        tf.to_float(self.model_train.rnn_outputs.shape[-1])
                     )
                 )
             )
@@ -54,7 +58,7 @@ class Trainer():
                     )),
                     tf.multiply(
                         tf.reduce_sum(self.model_train.expand_seq_masks[1:]),
-                        self.model_train.rnn_outputs.shape[-1]
+                        tf.to_float(self.model_train.rnn_outputs.shape[-1])
                     )
                 )
             )
@@ -62,6 +66,8 @@ class Trainer():
                 [self.raw_loss, self.activate_reg, self.temporal_activate_reg], name='all_loss')
             self.global_step = tf.Variable(
                 0, name="global_step", trainable=False)
+            self.learning_rate = tf.train.exponential_decay(self.learning_rate, self.global_step,
+                                                            self.decay_freq, self.decay_rate, staircase=False)
             self.optimizer = self.optimizer(self.learning_rate)
             self.grads, self.vars = zip(
                 *self.optimizer.compute_gradients(self.loss))
@@ -73,10 +79,10 @@ class Trainer():
             )
             # Add summary op
             self.ppl = tf.exp(self.raw_loss)
-            self.bpc = self.raw_loss/tf.log(2)
+            self.bpc = self.raw_loss/tf.log(2.0)
         train_summaries = [tf.summary.scalar('Loss', self.raw_loss),
                            tf.summary.scalar('Perplexity', self.ppl),
-                           tf.summary.scalar('Bit per character', self.bpc)]
+                           tf.summary.scalar('Bit_per_character', self.bpc)]
         self.train_summaries = tf.summary.merge(
             train_summaries, name='train_summaries')
         ema = tf.train.ExponentialMovingAverage(decay=0.998)
@@ -86,6 +92,7 @@ class Trainer():
             self.train_op = ema.apply(var_class)
         self.model_test = LanguageModel(
             **self.model_configs, reuse=True, is_training=False, custom_getter=get_getter(ema), name=self.model_train.name)
+        self.model_test.build_model()
         self.test_loss = tf.contrib.seq2seq.sequence_loss(
             logits=self.model_test.decoder,
             targets=self.y,
@@ -97,7 +104,7 @@ class Trainer():
         test_summaries = [tf.summary.scalar('Loss', self.test_loss),
                           tf.summary.scalar(
                               'Perplexity', tf.exp(self.test_loss)),
-                          tf.summary.scalar('Bit per character', self.test_loss/tf.log(2))]
+                          tf.summary.scalar('Bit_per_character', self.test_loss/tf.log(2.0))]
         self.test_summaries = tf.summary.merge(
             test_summaries, name='test_summaries')
         self.train_saver = tf.train.Saver(
@@ -169,3 +176,31 @@ class Trainer():
 
     def close(self):
         self.session.close()
+
+
+if __name__ == '__main__':
+    params = dict(
+        model_configs={
+            'rnn_layers': [
+                {'units': 1150, 'input_size': 400, 'drop_i': 0.5, 'drop_w': 0.4},
+                {'units': 1150, 'input_size': 1150, 'drop_w': 0.4},
+                {'units': 400, 'input_size': 1150, 'drop_o': 0.3, 'drop_w': 0.4}
+            ],
+            'vocab_size': 200,
+            'drop_e': 0.1
+        },
+        optimizer=tf.train.GradientDescentOptimizer,
+        learning_rate=30.0,
+        decay_rate=0.1,
+        decay_freq=100000,
+        alpha=1e-5,
+        beta=1e-5,
+        clip_norm=1.0,
+        bptt=200,
+        log_path='logs/',
+        train_summary_dir='train_summary/',
+        test_summary_dir='test_summary/',
+        checkpoint_dir='checkpoints/'
+    )
+    my_trainer = Trainer(**params)
+    my_trainer.build()
