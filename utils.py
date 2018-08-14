efaa88bfcfc67dc7af7aec5ad4b9a57ccf0d2286
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2018-08-14 09:27:57 +0700

    clean code

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def optimistic_restore(session, save_file):
     name2var = dict(zip(map(lambda x: x.name.split(
         ':')[0], tf.global_variables()), tf.global_variables()))
     with tf.variable_scope('', reuse=True):
-        for var_name, saved_var_name in var_names:
+        for _, saved_var_name in var_names:
             curr_var = name2var[saved_var_name]
             var_shape = curr_var.get_shape().as_list()
             if var_shape == saved_shapes[saved_var_name]:
