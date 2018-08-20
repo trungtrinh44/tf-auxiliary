14310d36cfac6bf16567a21cdcc9b2ebe26b5927
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2018-08-20 22:22:16 +0700

    trainer for biLM

--- trainer.py
+++ trainer.py
@@ -45,18 +45,6 @@ class Trainer():
         self.wdecay = wdecay
         self.ema_decay = ema_decay
 
-    def initialize_uninitialized(self):
-        global_vars = tf.global_variables()
-        is_not_initialized = self.session.run(
-            [tf.is_variable_initialized(var) for var in global_vars])
-        not_initialized_vars = [v for (v, f) in zip(
-            global_vars, is_not_initialized) if not f]
-
-        # only for testing
-        self.logger.info(str([str(i.name) for i in not_initialized_vars]))
-        if len(not_initialized_vars):
-            self.session.run(tf.variables_initializer(not_initialized_vars))
-
     def build(self):
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True  # pylint: disable=no-member
@@ -66,25 +54,47 @@ class Trainer():
         self.model_train.build_model()
         self.logger.info(self.model_train.variables)
         with tf.variable_scope(self.name):
-            self.y = tf.placeholder(dtype=tf.int32, shape=[
-                                    None, None], name='y')
+            self.fw_y = tf.placeholder(dtype=tf.int32, shape=[
+                None, None], name='fw_y')
+            self.bw_y = tf.placeholder(dtype=tf.int32, shape=[
+                None, None], name='bw_y')
             self.lr = tf.placeholder(dtype=tf.float32, shape=[], name='lr')
-            self.raw_loss = tf.contrib.seq2seq.sequence_loss(
-                logits=self.model_train.decoder,
-                targets=self.y,
-                weights=self.model_train.seq_masks,
-                average_across_timesteps=True,
-                average_across_batch=True,
+            self.raw_loss = 0.5 * tf.add(
+                tf.contrib.seq2seq.sequence_loss(
+                    logits=self.model_train.fw_model['decoder'],
+                    targets=self.fw_y,
+                    weights=self.model_train.seq_masks,
+                    average_across_timesteps=True,
+                    average_across_batch=True),
+                tf.contrib.seq2seq.sequence_loss(
+                    logits=self.model_train.bw_model['decoder'],
+                    targets=self.bw_y,
+                    weights=self.model_train.seq_masks,
+                    average_across_timesteps=True,
+                    average_across_batch=True),
                 name='train_loss'
             )  # Since we try the character model first, simple loss is the best
             if self.alpha > 0.0:
                 self.activate_reg = tf.multiply(
                     self.alpha,
-                    tf.div(
-                        tf.reduce_sum(tf.square(self.model_train.rnn_outputs)),
-                        tf.multiply(
-                            tf.reduce_sum(self.model_train.seq_masks),
-                            tf.to_float(self.model_train.rnn_outputs.shape[-1])
+                    tf.add(
+                        tf.div(
+                            tf.reduce_sum(
+                                tf.square(self.model_train.fw_model['rnn_outputs'])),
+                            tf.multiply(
+                                tf.reduce_sum(self.model_train.seq_masks),
+                                tf.to_float(
+                                    self.model_train.fw_model['rnn_outputs'].shape[-1])
+                            )
+                        ),
+                        tf.div(
+                            tf.reduce_sum(
+                                tf.square(self.model_train.bw_model['rnn_outputs'])),
+                            tf.multiply(
+                                tf.reduce_sum(self.model_train.seq_masks),
+                                tf.to_float(
+                                    self.model_train.bw_model['rnn_outputs'].shape[-1])
+                            )
                         )
                     )
                 )
@@ -93,16 +103,32 @@ class Trainer():
             if self.beta > 0.0:
                 self.temporal_activate_reg = tf.multiply(
                     self.beta,
-                    tf.div(
-                        tf.reduce_sum(tf.square(
-                            tf.subtract(
-                                self.model_train.rnn_outputs[1:],
-                                self.model_train.rnn_outputs[:-1]
+                    tf.add(
+                        tf.div(
+                            tf.reduce_sum(tf.square(
+                                tf.subtract(
+                                    self.model_train.fw_model['rnn_outputs'][1:],
+                                    self.model_train.fw_model['rnn_outputs'][:-1]
+                                )
+                            )),
+                            tf.multiply(
+                                tf.reduce_sum(self.model_train.seq_masks[1:]),
+                                tf.to_float(
+                                    self.model_train.fw_model['rnn_outputs'].shape[-1])
                             )
-                        )),
-                        tf.multiply(
-                            tf.reduce_sum(self.model_train.seq_masks[1:]),
-                            tf.to_float(self.model_train.rnn_outputs.shape[-1])
+                        ),
+                        tf.div(
+                            tf.reduce_sum(tf.square(
+                                tf.subtract(
+                                    self.model_train.bw_model['rnn_outputs'][1:],
+                                    self.model_train.bw_model['rnn_outputs'][:-1]
+                                )
+                            )),
+                            tf.multiply(
+                                tf.reduce_sum(self.model_train.seq_masks[1:]),
+                                tf.to_float(
+                                    self.model_train.bw_model['rnn_outputs'].shape[-1])
+                            )
                         )
                     )
                 )
@@ -146,22 +172,29 @@ class Trainer():
             self.model_test = LanguageModel(
                 **self.model_configs, reuse=True, is_training=False, custom_getter=get_getter(ema), name=self.model_train.name)
             self.test_saver = tf.train.Saver(
-                {v.op.name: ema.average(v) for v in var_class}, max_to_keep=1000
+                {v.op.name: ema.average(v) for v in var_class}, max_to_keep=100
             )
             self.ema = ema
         else:
             self.model_test = LanguageModel(
                 **self.model_configs, reuse=True, is_training=False, custom_getter=None, name=self.model_train.name)
             self.test_saver = tf.train.Saver(
-                tf.global_variables(), max_to_keep=1000
+                tf.global_variables(), max_to_keep=100
             )
         self.model_test.build_model()
-        self.test_loss = tf.contrib.seq2seq.sequence_loss(
-            logits=self.model_test.decoder,
-            targets=self.y,
-            weights=self.model_test.seq_masks,
-            average_across_timesteps=True,
-            average_across_batch=True,
+        self.test_loss = 0.5 * tf.add(
+            tf.contrib.seq2seq.sequence_loss(
+                logits=self.model_test.fw_model['decoder'],
+                targets=self.fw_y,
+                weights=self.model_test.seq_masks,
+                average_across_timesteps=True,
+                average_across_batch=True),
+            tf.contrib.seq2seq.sequence_loss(
+                logits=self.model_test.bw_model['decoder'],
+                targets=self.bw_y,
+                weights=self.model_test.seq_masks,
+                average_across_timesteps=True,
+                average_across_batch=True),
             name='test_loss'
         )
         test_summaries = [tf.summary.scalar('Loss', self.test_loss),
@@ -195,17 +228,19 @@ class Trainer():
         batch, i = 0, 0
         step = None
         while i < len(train_data)-1:
-            next_x, next_y = get_batch(train_data, self.bptt, i)
-            self.logger.info("Len {:4d}".format(len(next_x)))
+            (fw_x, fw_y), (bw_x, bw_y) = get_batch(train_data, self.bptt, i)
+            self.logger.info("Len {:4d}".format(len(fw_x)))
             _, loss, ppl, bpc, step, summaries = self.session.run(
                 [self.train_op, self.raw_loss, self.ppl, self.bpc,
                     self.global_step, self.train_summaries],
                 feed_dict={
                     self.lr: lr,
-                    model.inputs: next_x,
-                    self.y: next_y,
+                    model.fw_inputs: fw_x,
+                    model.bw_inputs: bw_x,
+                    self.fw_y: fw_y,
+                    self.bw_y: bw_y,
                     model.seq_lens: [
-                        next_x.shape[0]]*next_x.shape[1],
+                        fw_y.shape[0]]*fw_y.shape[1],
                     model.reset_state: i == 0
                 }
             )
@@ -220,7 +255,7 @@ class Trainer():
             )
             start_time = time.time()
             batch += 1
-            i += len(next_y)
+            i += len(fw_y)
             if step % self.save_freq == 0:
                 self.train_saver.save(
                     self.session, os.path.join(self.checkpoint_dir, folder_name, 'model.cpkt'), global_step=step)
@@ -232,19 +267,22 @@ class Trainer():
         total_loss = 0
         step = None
         for i in range(0, len(test_data), self.bptt):
-            next_x, next_y = get_batch(test_data, self.bptt, i, evaluate=True)
+            (fw_x, fw_y), (bw_x, bw_y) = get_batch(
+                test_data, self.bptt, i, evaluate=True)
             summaries, loss, step = self.session.run(
                 [self.test_summaries, self.test_loss, self.global_step],
                 feed_dict={
-                    model.inputs: next_x,
-                    self.y: next_y,
+                    model.fw_inputs: fw_x,
+                    model.bw_inputs: bw_x,
+                    self.fw_y: fw_y,
+                    self.bw_y: bw_y,
                     model.seq_lens: [
-                        next_x.shape[0]]*next_x.shape[1],
+                        fw_y.shape[0]]*fw_y.shape[1],
                     model.reset_state: i == 0
                 }
             )
             self.dev_summaries_writer.add_summary(summaries, step)
-            total_loss += loss * len(next_x)
+            total_loss += loss * len(fw_y)
             self.logger.info("Evaluate loss {}, time {}".format(
                 loss, time.time()-start_time))
         total_loss /= len(test_data)
@@ -278,134 +316,6 @@ class Trainer():
                         folder_name='fine_tune_train')
         self.evaluate_step(self.fine_tune_model, test_data,
                            folder_name='fine_tune_test')
-
-    def add_classifier(self, num_classes, classifier_configs):
-        self.num_classes = num_classes
-        self.logger.info('Classifier configs: {}'.format(classifier_configs))
-        # TODO: Add discriminative fine tuning
-        inputs = tf.stop_gradient(self.model_test.rnn_outputs)
-        inputs = tf.transpose(inputs, [1, 0, 2])
-        self.train_classifiers = [
-            Classifier(**classifier_configs, inputs=inputs,
-                       num_class=2, is_training=True, reuse=False, name='Classifier_' + str(i)) for i in range(num_classes)
-        ]
-        for c in self.train_classifiers:
-            c.build()
-        self.class_ys = [
-            tf.placeholder(dtype=tf.float32, shape=[None, 2], name='class_y'+str(i)) for i in range(num_classes)
-        ]
-        self.train_class_losses = [
-            tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_true, logits=c.outputs)) for y_true, c in zip(self.class_ys, self.train_classifiers)
-        ]
-        self.all_train_class_loss = tf.add_n(
-            self.train_class_losses, name='all_train_class_loss')
-        self.test_classifiers = [
-            Classifier(**classifier_configs, inputs=inputs,
-                       num_class=2, is_training=False, reuse=True, name='Classifier_' + str(i)) for i in range(num_classes)
-        ]
-        reg_loss = [x for c in self.train_classifiers for x in c.l2_reg_losses]
-        if len(reg_loss) > 0:
-            self.class_real_loss = tf.add(
-                self.all_train_class_loss,
-                tf.add_n(reg_loss),
-                name='class_real_loss'
-            )
-        else:
-            self.class_real_loss = tf.identity(
-                self.all_train_class_loss, name='class_real_loss')
-        for c in self.test_classifiers:
-            c.build()
-        self.test_class_losses = [
-            tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_true, logits=c.outputs)) for y_true, c in zip(self.class_ys, self.test_classifiers)
-        ]
-        self.all_test_class_loss = tf.add_n(
-            self.test_class_losses, name='all_test_class_loss')
-        self.test_class_saver = tf.train.Saver(
-            dict(**{v.op.name: v for c in self.test_classifiers for v in c.variables},
-                 **self.test_saver._var_list),
-            max_to_keep=1000
-        )
-        self.train_class_saver = tf.train.Saver(
-            tf.global_variables(),
-            max_to_keep=1
-        )
-        self.class_global_step = tf.Variable(
-            0, name="class_global_step", trainable=False)
-        self.class_grads, self.class_vars = zip(
-            *self.optimizer.compute_gradients(self.class_real_loss))
-        self.class_grads, _ = tf.clip_by_global_norm(
-            self.class_grads, clip_norm=self.clip_norm)
-        self.class_train_op = self.optimizer.apply_gradients(
-            zip(self.class_grads, self.class_vars),
-            global_step=self.class_global_step
-        )
-        self.initialize_uninitialized()
-
-    def classifier_train_dev_loop(self, train_gen, test_gen):
-        self.classifier_train_step(train_gen)
-        self.classifier_evaluate_step(test_gen)
-
-    def classifier_train_step(self, train_gen):
-        start_time = time.time()
-        step = None
-        for next_x, next_y, seq_len in train_gen:
-            fd = {
-                self.model_test.inputs: next_x,
-                self.model_test.seq_lens: seq_len,
-                self.model_test.reset_state: True
-            }
-            fd.update(
-                (k, v) for k, v in zip(self.class_ys, next_y)
-            )
-            _, loss, step = self.session.run(
-                [self.class_train_op, self.all_train_class_loss,
-                    self.class_global_step],
-                feed_dict=fd)
-            self.logger.info(
-                "Step {:4d}: loss {:05.5f}, time {:05.2f}".format(
-                    step,
-                    loss,
-                    time.time()-start_time)
-            )
-            start_time = time.time()
-            if step % self.save_freq == 0:
-                self.train_class_saver.save(
-                    self.session, os.path.join(self.checkpoint_dir, 'classifier', 'train', 'model.cpkt'), global_step=step)
-        self.train_class_saver.save(
-            self.session, os.path.join(self.checkpoint_dir, 'classifier', 'train', 'model.cpkt'), global_step=step)
-
-    def classifier_evaluate_step(self, test_gen):
-        start_time = time.time()
-        step = None
-        total_loss = 0.0
-        batch = 0
-        for i, (next_x, next_y, seq_len) in enumerate(test_gen):
-            fd = {
-                self.model_test.inputs: next_x,
-                self.model_test.seq_lens: seq_len,
-                self.model_test.reset_state: True
-            }
-            fd.update(
-                (k, v) for k, v in zip(self.class_ys, next_y)
-            )
-            loss, step = self.session.run(
-                [self.all_test_class_loss,
-                    self.class_global_step],
-                feed_dict=fd)
-            self.logger.info(
-                "Evaluate step {:4d}: loss {:05.5f}, time {:05.2f}".format(
-                    step,
-                    loss,
-                    time.time()-start_time)
-            )
-            total_loss += len(next_x) * loss
-            batch += len(next_x)
-        self.test_class_saver.save(
-            self.session, os.path.join(self.checkpoint_dir, 'classifier', 'test', 'model.cpkt'), global_step=step)
-        self.logger.info(
-            'Evaluate total loss: {}, avg. time: {}'.format(total_loss / batch,
-                                                            (time.time()-start_time) / i)
-        )
 
 
 if __name__ == '__main__':
