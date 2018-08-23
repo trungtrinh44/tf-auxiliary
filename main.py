ba35d57c59c45b9bfbf0146ebae805b890ac5d83
Trung <trinhtrung96@gmail.com>
2018-08-23 05:55:13 +0000

    main

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 from data import Corpus
 import trainer
 from utils import batchify
@@ -11,101 +5,60 @@ import json
 import tensorflow as tf
 import numpy as np
 
-# In[2]:
 
-
-# corpus = Corpus('baomoi/')
-
-
-# In[3]:
-
-
-# In[4]:
-
-
-# np.save('baomoi/train.npy', corpus.train)
-
-
-# In[5]:
-
-
-# np.save('baomoi/valid.npy', corpus.valid)
-
-
-# In[6]:
-
-
-# np.save('baomoi/test.npy', corpus.test)
-
-
-# In[7]:
-
-
-# with open('word2idx.json','w') as out:
-#     json.dump(corpus.dictionary.word2idx, out)
-
-
-# In[8]:
-
-
-with open('baomoi_punc/word2idx.json','r') as inp:
+with open('baomoi_punc/word2idx.json', 'r') as inp:
     word2idx = json.load(inp)
+with open('baomoi_punc/char2idx.json', 'r') as inp:
+    char2idx = json.load(inp)
 
 
-# In[9]:
-
-
-with open('baomoi_punc/train.npy','rb') as inp:
-    train = np.load(inp)
-with open('baomoi_punc/test.npy','rb') as inp:
-    test = np.load(inp)
-with open('baomoi_punc/valid.npy','rb') as inp:
-    valid = np.load(inp)
-
-
-# In[10]:
-
-
-train_data = batchify(train, 202).T
-val_data = batchify(valid, 158).T
-
-
-# In[11]:
-
-
-VERSION = 7
+VERSION = 14
 params = dict(
     model_configs = {
-   "rnn_layers":[
+      "rnn_layers":[
           {
-             "units": 1200,
-             "input_size":400,
+             "units": 1024,
              "drop_i": 0.01,
-             "wdrop": 0.02,
+             "wdrop": 0.05,
              "drop_o": 0.01
           },
           {
-             "units": 1200,
-             "input_size": 1200,
-             "wdrop": 0.02,
+             "units": 1024,
+             "wdrop": 0.05,
              "drop_o": 0.01
           },
           {
-             "units": 400,
-             "input_size": 1200,
-             "drop_o": 0.1,
-             "wdrop": 0.02
+             "units": 1024,
+             "drop_o": 0.04,
+             "wdrop": 0.05
           }
        ],
-       "vocab_size": len(word2idx),
-       "drop_e":0.0
+       "vocab_size": len(word2idx) + 1,
+       "drop_e": 0.0,
+       "char_vocab_size": len(char2idx) + 1,
+       "char_cnn_options": {
+           "layers": [
+                [1, 16],
+                [2, 16],
+                [3, 32],
+                [4, 64],
+                [5, 128],
+                [6, 256],
+                [7, 512]
+           ],
+           "n_highways": 2
+       },
+       "char_vec_size": 16,
+       "projection_dims": 512,
+       "skip_connection": True
     },
     optimizer = tf.train.GradientDescentOptimizer,
+    negative_samples = 10240,
     wdecay = 1.2e-6,
     alpha = 0.0,
     beta = 0.0,
     clip_norm = 0.3,
-    bptt = 200,
+    bptt = 100,
     use_ema = True,
     save_freq = 1000,
     log_path = '{}/logs'.format(VERSION),
@@ -127,10 +80,21 @@ my_trainer.logger.info('Trainer params {}'.format(params))
 tf.reset_default_graph()
 my_trainer.build()
 
+with open('baomoi_punc/train_word.npy','rb') as inp:
+    train_word = np.load(inp)
+with open('baomoi_punc/train_char.npy','rb') as inp:
+    train_char = np.load(inp)
+with open('baomoi_punc/valid_word.npy','rb') as inp:
+    valid_word = np.load(inp)
+with open('baomoi_punc/valid_char.npy','rb') as inp:
+    valid_char = np.load(inp)
 
-# In[ ]:
+train_word = batchify(train_word, 41).T
+train_char = batchify(train_char, 41).T
+valid_word = batchify(valid_word, 57).T
+valid_char = batchify(valid_char, 57).T
 
-lr = 5.0
+lr = 10.0
 for _ in range(10000):
-    my_trainer.train_dev_loop(train_data, val_data, lr)
-    lr = max(lr / np.sqrt(2.0), 1e-2)
+    my_trainer.train_dev_loop(train_word, train_char, valid_word, valid_char, lr)
+    lr = max(lr / np.sqrt(2.0), 2.0)
