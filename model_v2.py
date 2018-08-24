385fae8dba7596d6390b89ce331aa6f5eb43f96d
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2018-08-24 14:59:35 +0700

    fix stupid error

--- model_v2.py
+++ model_v2.py
@@ -289,7 +289,7 @@ class LanguageModel():
                 indices = tf.range(start=0, limit=tf.shape(self.seq_lens)[0], delta=1, dtype=tf.int32)
                 indices = tf.stack((self.seq_lens - 1, indices), axis=-1)
                 self.encode_outputs = []
-                for idx, fw, bw in enumerate(zip(self.fw_model['layer_outputs'], self.bw_model['layer_outputs'])):
+                for idx, (fw, bw) in enumerate(zip(self.fw_model['layer_outputs'], self.bw_model['layer_outputs'])):
                     bw = tf.reverse_sequence(
                         input=bw,
                         seq_lengths=self.seq_lens,
