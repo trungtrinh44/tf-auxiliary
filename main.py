5adac4719928dfefcc4b3cb591aedb7cdd1085bf
Trinh Quoc Trung <trungtq@younetgroup.com>
2018-12-14 08:19:32 +0100

    add main

--- main.py
+++ main.py
@@ -6,35 +6,35 @@ import tensorflow as tf
 import numpy as np
 
 
-with open('baomoi_punc/word2idx.json', 'r') as inp:
+with open('wiki/word2idx.json', 'r') as inp:
     word2idx = json.load(inp)
-with open('baomoi_punc/char2idx.json', 'r') as inp:
+with open('wiki/char2idx.json', 'r') as inp:
     char2idx = json.load(inp)
 
 
-VERSION = 14
+VERSION = 102
 params = dict(
     model_configs = {
       "rnn_layers":[
           {
              "units": 1024,
-             "drop_i": 0.01,
-             "wdrop": 0.05,
-             "drop_o": 0.01
+             "drop_i": 0.1,
+             "wdrop": 0.4,
+             "drop_o": 0.1
           },
           {
              "units": 1024,
-             "wdrop": 0.05,
-             "drop_o": 0.01
+             "wdrop": 0.4,
+             "drop_o": 0.1
           },
           {
              "units": 1024,
-             "drop_o": 0.04,
-             "wdrop": 0.05
+             "drop_o": 0.2,
+             "wdrop": 0.4
           }
        ],
        "vocab_size": len(word2idx) + 1,
-       "drop_e": 0.0,
+       "drop_e": 0.1,
        "char_vocab_size": len(char2idx) + 1,
        "char_cnn_options": {
            "layers": [
@@ -55,10 +55,10 @@ params = dict(
     optimizer = tf.train.GradientDescentOptimizer,
     negative_samples = 10240,
     wdecay = 1.2e-6,
-    alpha = 0.0,
-    beta = 0.0,
+    alpha = 1e-6,
+    beta = 1e-6,
     clip_norm = 0.3,
-    bptt = 100,
+    bptt = 70,
     use_ema = True,
     save_freq = 1000,
     log_path = '{}/logs'.format(VERSION),
@@ -80,19 +80,19 @@ my_trainer.logger.info('Trainer params {}'.format(params))
 tf.reset_default_graph()
 my_trainer.build()
 
-with open('baomoi_punc/train_word.npy','rb') as inp:
+with open('wiki/train_word.npy','rb') as inp:
     train_word = np.load(inp)
-with open('baomoi_punc/train_char.npy','rb') as inp:
+with open('wiki/train_char.npy','rb') as inp:
     train_char = np.load(inp)
-with open('baomoi_punc/valid_word.npy','rb') as inp:
+with open('wiki/valid_word.npy','rb') as inp:
     valid_word = np.load(inp)
-with open('baomoi_punc/valid_char.npy','rb') as inp:
+with open('wiki/valid_char.npy','rb') as inp:
     valid_char = np.load(inp)
 
-train_word = batchify(train_word, 41).T
-train_char = batchify(train_char, 41).T
-valid_word = batchify(valid_word, 57).T
-valid_char = batchify(valid_char, 57).T
+train_word = batchify(train_word, 68).T
+train_char = batchify(train_char, 68).T
+valid_word = batchify(valid_word, 72).T
+valid_char = batchify(valid_char, 72).T
 
 lr = 10.0
 for _ in range(10000):
